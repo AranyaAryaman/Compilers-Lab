7e6814c903f0c1c7f481b891235c0a5b9b8aa3a8
AranyaAryaman <aryamanaranya@gmail.com>
2020-01-26 05:45:09 +0530

    Bug-Fixes & References

--- refactor/src/code_gen.c
+++ refactor/src/code_gen.c
@@ -168,3 +168,169 @@ char *factor(void) {
 
 	return tempvar;
 }
+
+
+// statements()
+// {
+// 		/*statements -> statements statement | statement
+
+
+// 	assFile = fopen("Assembly.asm", "w");
+// 	interFile = fopen("Intermediate.txt", "w");
+
+// 	fprintf(assFile, "%s\n", "ORG 0000h");
+
+// 	while(!match(EOI))
+// 		statement();
+
+// 	fprintf(assFile, "%s\n", "END");
+// 	fclose(assFile);
+// 	fclose(interFile);
+// }
+
+// statement()
+// {
+// 	/*  statement -> ID:=expression1; | IF expression1 then statement |
+// 		while expression1 do statement | expression1; |
+// 		begin  stmt_list  end |
+// 		begin end
+// 	  */
+
+// 	char *tempvar;
+
+
+// 		if(match(ID))
+// 		{
+// 			char assignment[100];
+// 			int loop_var=0;
+// 			for(;loop_var<yyleng;loop_var++)assignment[loop_var]=*(yytext+loop_var);
+// 			assignment[loop_var]='\0';
+
+// 			advance();
+// 			if( !legal_lookahead( COL, 0 ) )
+// 				goto legal_lookahead_SEMI;
+// 			if(match(COL))
+// 			{
+// 				advance();
+// 				if( !legal_lookahead( EQUAL, 0 ) )
+// 					goto legal_lookahead_SEMI;
+// 				if(match(EQUAL))
+// 				{
+// 					advance();
+// 					tempvar = expression1();
+// 					fprintf(interFile,"%s <- %s\n",assignment,tempvar);
+
+// 					if(strcmp(tempvar, "t0") == 0){
+// 						fprintf(assFile, "STA _%s\n", assignment);
+// 					} else {
+// 						fprintf(assFile, "PUSH A\nMOV A %c\nSTA _%s\nPOP A\n", REG[tempvar[1]-'0'], assignment);
+// 					}
+// 				}
+// 				else
+// 				{
+// 					fprintf( stderr, "%d: Inserting missing equal\n", yylineno );
+// 				}
+// 			}
+// 			else
+// 			{
+// 				fprintf( stderr, "%d: Inserting missing colon\n", yylineno );
+// 			}
+// 		}
+// 		else if(match(IF))
+// 			{
+// 				advance();
+// 				fprintf(interFile, "if (\n");
+// 				tempvar = expression1();
+// 				if( !legal_lookahead( THEN, 0 ) )
+// 				{
+// 					freename(tempvar);
+// 					fprintf( stderr, "%d: Inserting missing then\n", yylineno );
+// 					goto legal_lookahead_SEMI;
+// 				}
+// 				if(match(THEN))
+// 				{
+// 					fprintf(interFile, "%s)\n", tempvar);
+// 					int ifthenLabel = getIfThenLabel();
+// 					fprintf(assFile, "CMP %c 0\nJZ IFTHEN%d\n", REG[tempvar[1] -'0'], ifthenLabel);
+// 					fprintf(interFile, "%s\n", "then {");
+// 					advance();
+// 					freename(tempvar);
+// 					statement();
+// 					fprintf(interFile, "%s\n", "}");
+// 					fprintf(assFile, "IFTHEN%d:\n", ifthenLabel);
+// 					return;
+// 				}
+// 				else
+// 					fprintf( stderr, "%d: Inserting missing then\n", yylineno );
+// 			}
+// 		else if(match(WHILE))
+// 			{
+// 				advance();
+// 				int loopLabel1 = getLoopLabel();
+// 				fprintf(assFile, "LOOP%d:\n",loopLabel1);
+// 				fprintf(interFile, "while (\n");
+// 				tempvar = expression1();
+// 				if( !legal_lookahead( DO, 0 ) )
+// 				{
+// 					freename(tempvar);
+// 					goto legal_lookahead_SEMI;
+// 				}
+// 				if(match(DO))
+// 				{
+// 					fprintf(interFile, "%s)\n", tempvar);
+// 					int loopLabel = getLoopLabel();
+// 					fprintf(assFile, "CMP %c 0\nJZ LOOP%d\n", REG[tempvar[1]-'0'], loopLabel);
+
+// 					fprintf(interFile, "%s\n", "do {");
+// 					advance();
+
+// 					freename(tempvar);
+// 					statement();
+// 					fprintf(interFile, "%s\n", "}");
+// 					fprintf(assFile, "JMP LOOP%d\nLOOP%d:\n", loopLabel1, loopLabel);
+// 					// freename(tempvar);
+// 					return;
+// 				}
+// 				else
+// 					fprintf( stderr, "%d: Inserting missing do\n", yylineno );
+
+// 			}
+// 		else if(match(BEGIN))
+// 		{
+
+// 				fprintf(interFile, "%s\n", "BEGIN{");
+// 				advance();
+// 				if(match(END)){printf("END\n");advance();return;}
+// 				stmt_list();
+// 				if(!legal_lookahead(END,0))
+// 				{
+// 					fprintf( stderr, "%d: Inserting missing END\n", yylineno );
+// 					goto legal_lookahead_SEMI;
+// 				}
+
+// 				fprintf(interFile, "} %s\n", "END");
+// 				advance();
+// 				return;
+// 		}
+// 		else tempvar = expression1();
+
+// 		if(tempvar!=NULL)freename( tempvar );
+// 		else exit(1);
+
+// legal_lookahead_SEMI:
+// 		if( match( SEMI ) )
+// 			advance();
+// 		else
+// 			fprintf( stderr, "%d: Inserting missing semicolon\n", yylineno );
+// }
+
+// stmt_list()
+// {
+
+// 	/*stmt_list -> statement stmt_list*/
+
+// 	while(!match(END)&&!match(EOI))
+// 		statement();
+// 	if(match(EOI))fprintf( stderr, "%d: End of file reached no END found\n", yylineno );
+// }
+// */
